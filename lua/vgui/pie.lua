99e060fc956a205630c3a443f62495d695f94084
akersda <63028728+akersda@users.noreply.github.com>
2020-07-16 21:12:13 +0930

    outlines
    added background/outlining feature
    
    note: internal lines need to be fixed

--- lua/vgui/pie.lua
+++ lua/vgui/pie.lua
@@ -3,12 +3,16 @@ local PANEL = {}
 function PANEL:Init()
 	
 	self.gdata = {}
+	self.backg = {}
 	self.showname = false
 	self.numsum = 0
 	self.proc = 361
-	self.radius1 = 100
-	self.radius2 = 200
+	self.radius1 = 20
+	self.radius2 = 40
 	self.speed = 15
+	self.drawback = false
+	self.backcol = Color( 0,0,0,255 )
+	self.backthick = 1
 	
 	self.thinktick = CurTime()
 	
@@ -16,12 +20,26 @@ end
 
 function PANEL:Paint( w, h )
 	
+	draw.NoTexture()
+	
+	if !table.IsEmpty(self.backg) and self.drawback == true then
+		surface.SetDrawColor(self.backcol)
+		draw.PartCircle( self.backg.cir1, self.backg.cir2 )
+	end
+	
 	if !table.IsEmpty(self.gdata) then
-		draw.NoTexture()
 		for k, seg in ipairs( self.gdata ) do
 			if seg.cir1 != nil and !table.IsEmpty(seg.cir1) then
 				surface.SetDrawColor(seg.col)
 				draw.PartCircle( seg.cir1, seg.cir2 )
+				if self.drawback then
+					surface.SetDrawColor(self.backcol)
+					surface.DrawLine( seg.cir1[1].x, seg.cir1[1].y, seg.cir2[1].x, seg.cir2[1].y )
+					surface.DrawLine( seg.cir1[#seg.cir1].x, seg.cir1[#seg.cir1].y, seg.cir2[#seg.cir2].x, seg.cir2[#seg.cir2].y )
+					if self.backthick > 1 then
+						surface.DrawTexturedRectRotated( (seg.cir1[1].x + seg.cir2[1].x) / 2, (seg.cir1[1].y + seg.cir2[1].y) / 2, self.backthick, self.radius2-self.radius1, seg.ang )
+					end
+				end
 			end
 		end
 	end
@@ -72,18 +90,47 @@ function PANEL:SetThick( num )
 	
 end
 
+function PANEL:SetBackground( col )
+	
+	if col == nil then
+		self.drawback = false
+	elseif IsColor(col) then
+		self.drawback = true
+		self.backcol = col
+	else
+		self.drawback = true
+		self.backcol = Color( 0,0,0,255 )
+	end
+	
+end
+
+function PANEL:SetBackThick( num )
+	
+	self.backthick = tonumber( num )
+	
+end
+
 function PANEL:Think()
 	
 	if self.thinktick < CurTime() then
-		self.thinktick = CurTime() + 0.0333 -- 30 fps
-		local w, h = self:GetWide()/2, self:GetTall()/2
+		self.thinktick = CurTime() + 0.0333 -- ~30 fps
+		local w, h = self:GetWide()/2, self:GetTall()/2 -- center
 		
 		if self.proc <= 360 then
 			local pos = 0
 			for k, entry in ipairs( self.gdata ) do
-				local sw = ( entry.data / self.numsum ) * self.proc
-				self.gdata[k].cir1, self.gdata[k].cir2 = draw.CalcVertsPartCir( w, h, self.radius1, self.radius2, pos, pos + sw )
+				local sw = ( entry.data / self.numsum ) * self.proc -- degrees of segment
+				if self.drawback then
+					self.gdata[k].cir1, self.gdata[k].cir2 = draw.CalcVertsPartCir( w, h, self.radius1 + self.backthick, self.radius2 - self.backthick, pos, pos + sw )
+					self.gdata[k].ang = pos
+				else
+					self.gdata[k].cir1, self.gdata[k].cir2 = draw.CalcVertsPartCir( w, h, self.radius1, self.radius2, pos, pos + sw )
+					self.gdata[k].ang = pos
+				end
 				pos = pos + sw
+			end
+			if self.drawback then
+				self.backg.cir1, self.backg.cir2 = draw.CalcVertsPartCir( w, h, self.radius1, self.radius2, 0, pos )
 			end
 			self.proc = self.proc + self.speed
 		end
